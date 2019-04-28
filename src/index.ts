4ac36b20ad566e48306a05214997a70ef27e2544
Drhell <no.anything.518@gmail.com>
2019-04-29 01:25:51 +0800

    feat(*): 增加ts下直接调试的功能，增加seq-ts模块与例子。

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import * as express from 'express';
 import { AdResponse } from './domain/AdResponse';
 import { ResponseBase } from './domain/ResponseBase';
+import { Ad } from './models/Ad';
+
 
 
 const app = express(); // 用于声明服务器端所能提供的http服务
 
+app.get('/', (req, res) => {
+    res.send(__dirname);
+});
+
 app.get('/test', (req, res) => {
     res.send('这是test页面');
-})
+});
 
 app.post('/test', (req, res) => {
     let adResponse = new AdResponse();
@@ -18,7 +24,15 @@ app.post('/test', (req, res) => {
     let responseBase = new ResponseBase(adResponse, 200);
 
     res.send(responseBase);
-})
+});
+
+/**
+ * 测试数据访问
+ */
+app.get('/data', async (req, res) => {
+    let datas = await Ad.findAll();
+    res.send(datas);
+});
 
 const server = app.listen(8888, "localhost", () => {
     console.log("服务器已启动, 地址是：http://localhost:8888");
